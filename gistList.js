8037e9166bc10c03cf38f0c10386b8a0345b6cd2
Matthew Chung <chungm@onid.oregonstate.edu>
2015-04-26 03:33:02 -0400

    Store the fetched gists into local storage, then print them out on the html screen.

--- gistList.js
+++ gistList.js
@@ -1,3 +1,4 @@
+var settings = null;
 
 function gistObj(descript, urlLink) {
 	this.descript = descript;
@@ -16,14 +17,16 @@ var fetchdata = function(){
 	
 	req.onreadystatechange = function(){ 
 		if(this.readyState === 4){
-			var originalGistList = [];
+			
       		var gistLog = JSON.parse(this.responseText);
 
       		for(var i = 0; i < 30; i++) {
-      			originalGistList.push(new gistObj(gistLog[i].description, gistLog[i].url));
-      			
+      			settings.originalGistList.push(new gistObj(gistLog[i].description, gistLog[i].url));
       		}
 
+
+      		localStorage.setItem('originalGistList', JSON.stringify(settings));
+      		createGistsList(document.getElementById('gist-list'));
 
 		}
 	};
@@ -32,17 +35,48 @@ var fetchdata = function(){
 	req.send();
 }
 
+function createGistsList(ul) {
+
+	settings.originalGistList.forEach(
+
+		function(s) {
+			var li = document.createElement('li');
+			li.appendChild(liGist(s));
+			ul.appendChild(li);
+
+		}
+	);
+}
+
+function liGist(g) {
+	var dl = document.createElement('dl');
+	var entry = dlEntry(g.descript, g.urlLink);
+	dl.appendChild(entry.dt);
+	dl.appendChild(entry.dd);
+	return dl;
+}
+
+function dlEntry(term, definition) {
+	var dt = document.createElement('dt');
+	var dd = document.createElement('dd');
+	
+	dt.innerText = term;
+	dd.innerText = definition;
+	return {'dt':dt, 'dd':dd};
+}
 
 
 window.onload = function() {
-	/*var settingsStr = localStorage.getItem('userSettings');
+	localStorage.clear('originalGistList');
+	
+	var settingsStr = localStorage.getItem('originalGistList');
 	if( settingsStr === null ) {
-		settings = {'sports':[]};
-		localStorage.setItem('userSettings', JSON.stringify(settings));
+		settings = {'originalGistList':[]};
+		localStorage.setItem('originalGistList', JSON.stringify(settings));
 	}
 	else {
-		settings = JSON.parse(localStorage.getItem('userSettings'));
-	} */
+		settings = JSON.parse(localStorage.getItem('originalGistList'));
+	} 
 
 	fetchdata();
 } 
